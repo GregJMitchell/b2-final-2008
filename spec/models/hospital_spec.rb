a4ec84a84d2213e33fe87c7245f8b5b5f3d1554f
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-09 10:06:14 -0700

    Create tests for new instance methods for hospital

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,7 +1,33 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 describe Hospital, type: :model do
   describe 'relationships' do
-    it { should have_many :doctors}
+    it { should have_many :doctors }
+  end
+
+  describe 'instance methods' do
+    it 'doctors_count' do
+      hospital = Hospital.create!(name: 'Mercy')
+
+      doctor = Doctor.create(name: 'Dr. Mike', specialty: 'I.D.', university: 'Hopkins', hospital_id: hospital.id)
+      doctor2 = Doctor.create(name: 'Dr. Kat', specialty: 'I.D.', university: 'Hopkins', hospital_id: hospital.id)
+      doctor3 = Doctor.create(name: 'Dr. Meg', specialty: 'I.D.', university: 'Harvard', hospital_id: hospital.id)
+      doctor4 = Doctor.create(name: 'Dr. Brian', specialty: 'I.D.', university: 'NYU', hospital_id: hospital.id)
+
+      expect(hospital.doctors_count).to eq(4)
+    end
+
+    it 'doctors_universities' do
+      hospital = Hospital.create!(name: 'Mercy')
+
+      doctor = Doctor.create(name: 'Dr. Mike', specialty: 'I.D.', university: 'Hopkins', hospital_id: hospital.id)
+      doctor2 = Doctor.create(name: 'Dr. Kat', specialty: 'I.D.', university: 'Hopkins', hospital_id: hospital.id)
+      doctor3 = Doctor.create(name: 'Dr. Meg', specialty: 'I.D.', university: 'Harvard', hospital_id: hospital.id)
+      doctor4 = Doctor.create(name: 'Dr. Brian', specialty: 'I.D.', university: 'NYU', hospital_id: hospital.id)
+
+      expect(hospital.doctors_universities).to eq(%w[Harvard Hopkins NYU])
+    end
   end
 end
