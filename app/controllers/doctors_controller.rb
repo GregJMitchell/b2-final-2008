85fae9a05dcb91039947080c7c7d80be39b59d83
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-09 10:35:04 -0700

    Create the remove patient action

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,4 +2,11 @@ class DoctorsController < ApplicationController
   def show
     @doctor = Doctor.find(params[:id])
   end
+
+  def remove_patient
+    doctor = Doctor.find(params[:id])
+    doctor_patient = doctor.find_patient_record(params[:patient_id])
+    doctor_patient.destroy
+    redirect_to "/doctors/#{params[:id]}"
+  end
 end
