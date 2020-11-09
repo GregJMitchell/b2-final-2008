1b7038da748f6fdfb7457401bb746ed0d296363b
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-09 10:33:59 -0700

    Create route for removing a patient

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :doctors, only: [:show]
+  delete "/doctors/:id/remove_patient", to: "doctors#remove_patient"
+
   resources :hospitals, only: [:show]
 end
