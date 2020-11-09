20de806a8492dda05078e41e74c5a2390d562de9
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-09 09:41:03 -0700

    Update model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor < ApplicationRecord
-  belongs_to :hospital, class_name: "hospital", foreign_key: "hospital_id"
+  belongs_to :hospital
 
   has_many :doctor_patients
   has_many :patients, through: :doctor_patients
