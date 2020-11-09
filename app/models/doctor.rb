477c3ea6663e665169c3225f6eb319cde153f6cb
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-09 10:34:29 -0700

    Create find_patient_record method to find doctor_patient record

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,8 @@ class Doctor < ApplicationRecord
 
   has_many :doctor_patients
   has_many :patients, through: :doctor_patients
+
+  def find_patient_record(patient_id)
+    doctor_patients.find_by(patient_id: patient_id)
+  end
 end
