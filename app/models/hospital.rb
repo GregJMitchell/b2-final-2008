7072534df5b9a7e1367d627bbced1efa7dcd6fd6
GregJMitchell <g2mitchell3003@gmail.com>
2020-11-09 10:07:32 -0700

    Create doctors_count and doctors_universities methods

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,11 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctors_count
+    doctors.count(:id)
+  end
+
+  def doctors_universities
+    doctors.select(:university).distinct.pluck(:university)
+  end
 end
